0f6fc382d7810cbc295c985c265fe7003dfb48dd
RobertTorres17 <49485666+RobertTorres17@users.noreply.github.com>
2019-04-10 09:42:19 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,20 @@
+
+    $(".clickme").click(function(){
+        $(".clickme").text("Put your mouse on 3");
+    });    
+         $(".typeonme").hover(function(){
+        $(".typeonme").text("Put your mouse on 4");
+    });    
+         $(".hoverme").mouseenter(function(){
+        $(".hoverme").text("Double click on 2");
+    });    
+        $(".doubleclick").dblclick(function(){
+        $(".doubleclick").text("Click on the image");
+    }); 
+    
+        $(".first_pic").click(function(){
+        $(".first_pic").hide("Click on the image");
+    }); 
+    
+    
+    
